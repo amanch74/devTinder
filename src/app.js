808dd046b6bb8272698c469784e0e99c58d65b54
user.name like : "amanchy2002@gmail.com <amanchy2002@gmail.com>
2026-06-12 00:04:40 +0530

    exploring the HTTP REQUEST(get,post and delete)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use("/test", (req, res) => {
+// This is the root route handler, anything that matches after "/" will trigger this handler. if order of routes is chaged then it will follow the order. so the order is important in express. if we put this route at the end then it will work as a catch all route and will trigger for any route that is not defined above it.
+// app.use("/", (req,res) => {
+//     res.send('Hello World!');
+// })
+
+app.use("/test", (req, res) => { // app.use() will match all the HTTP meethods
     res.send('Hello from Express!');
 });
 
-app.use("/hello", (req, res) => {
-    res.send('Hello hello hello!');
+// app.use("/hello", (req, res) => {
+//     res.send('Hello hello hello!');
+// });
+
+app.get("/user" , (req,res) => {
+    res.send("firstName: Aman, LastName: Choudhary");
+})
+
+app.post("/user", (req,res) => {
+    res.send("User created successfully!");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully!");
 });
 
 app.listen(3000, () => {
