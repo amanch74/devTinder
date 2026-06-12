378032bddd2dbad13c1d7675f608c1c4ee1b45b0
user.name like : "amanchy2002@gmail.com <amanchy2002@gmail.com>
2026-06-12 14:38:39 +0530

    middleware and route handler

--- src/app.js
+++ src/app.js
@@ -1,35 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use("/route", rH1, rH2, [rH3, rH4, rH5], rH6); // placing array individual or in an array will work same(place anyhow work in the order of the handlers). it wiil work on all http route handlers get, post, put, delete etc. it will work on all the routes that start with /route
+// GET /user => It checks all the app.xxx("matching routes") functions
+// GET /user => middlewares => request handler
+// actually the request handler function is the one which hits the request and sends back response and all the functions between them are middlewares
+app.use("/", (req,res, next) => {
+    // res.send("Handling /route");
+    next();
+})
 
-app.use("/user", (req, res, next) => {
-    // route handler
+app.get("/user", (req, res, next) => {
     console.log("Handling the route user1")
-    // next();
-    // res.send("Response1");
-    next(); // Call next() to pass control to the next route handler
-
-}, (req,res,next) => {
-    //second route handler
-    console.log("Handling the route user2");
-    // res.send("Response2"); // it will not come to this response until next() is called in the first route handler
     next();
-
-}, (req,res,next) => {
-    
-    console.log("Handling the route user3");
-    // res.send("Response3");
-    next();
-     
-    
-}, (req,res,next) => {
-    
-    console.log("Handling the route user4");
-    // res.send("Response4"); 
-    next(); // if the last handler has next( but no respnse) then it will move to the next route handller if there is nothing in the next i will throw an error cannot/Get. but if there is something next after and there is no response then it will show only loading...
-    
 });
+// one more way to create the route handler for the same route. It exactly matches and executes in the same way as previous one.
+app.get("/user", (req, res, next) => {
+    console.log("Handling the route user2")
+    res.send("Response2");
+});
+
 
 
 app.listen(3000, () => {
