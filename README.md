1dac3e0645e6d2073d27598c417447787c617296
user.name like : "amanchy2002@gmail.com <amanchy2002@gmail.com>
2026-06-17 23:26:55 +0530

    get-Apis

---
+++ README.md
@@ -0,0 +1,7 @@
+- difference between json and javasript object
+- Add the express.json middleware to your app
+- Make your signup Api dynamic to recieve data from the end user
+
+- first fetch the one user using userEmail = req.body.emailId and model.find({emailId : userEmail})
+- find all the users than
+- which user will it return on model.findOne
--- src/app.js
+++ src/app.js
@@ -3,21 +3,58 @@ const express = require('express');
 const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user");
+const { model } = require('mongoose');
 
-app.post("/signup", async (req,res) => {
-    const user = new User({
-        firstName : "vinita",
-        lastName : "singh",
-        emailId : "vinita@singh.com",
-        password : "vinita@123"
-    });
+//middleware given by express to read to json format and and convert it into the javascript object
+app.use(express.json());
 
-    try{
+app.post("/signup", async (req, res) => {
+
+    // console.log(req.body);
+    // creating an instance of the user model
+    const user = new User(req.body);
+
+    try {
         await user.save();
-    res.send("user added successfully");
+        res.send("user added successfully");
     }
-    catch(err){
+    catch (err) {
         res.status(400).send("Error saving the data" + err.message);
+    }
+})
+
+// get user by email
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+
+
+    try {
+        const user = await User.findOne({ emailId: userEmail }); // it will return the one document
+        res.send(user);
+        // const user = await User.find({ emailId: userEmail });
+        // if(user.length === 0){
+        //     res.status(404).send("user not found");
+        // }
+        // else{
+        //     res.send(user);
+        // }
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+
+    try {
+        const users = await User.find({});// by initializing empty it give all the users from the document.
+        res.send(users);
+    }
+    catch (err) {
+        res.status(400).send("Something went wrong")
     }
 })
 
